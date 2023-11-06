dd60426b883f82e711637ff1a13d9a868695104a
Matthias Kastner <matthias.kastner@outlook.com>
2023-11-06 16:25:44 +0100

    mosquitto goes brr

--- Steuereinheit/steuereinheit.py
+++ Steuereinheit/steuereinheit.py
@@ -1,47 +1,56 @@
-import time
-import paho.mqtt.client as paho
-from paho import mqtt
+import cv2 as cv
+import paho.mqtt.client as mqtt
+import numpy as np
 
-# setting callbacks for different events to see if it works, print the message etc.
-def on_connect(client, userdata, flags, rc, properties=None):
-    print("Steuereinheit received with code %s." % rc)
+# MQTT broker address and port
+broker_address = "localhost"  # Replace this with your broker's address if it's different
+port = 1883  # Default MQTT port
 
-# with this callback you can see if your publish was successful
-def on_publish(client, userdata, mid, properties=None):
-    print("mid: " + str(mid))
+# Topic to which you want to publish the message
+topic1 = "Steuereinheit/befehle"
+topic2 = "Steuereinheit/kennzeichen_foto"
 
-# print which topic was subscribed to
-def on_subscribe(client, userdata, mid, granted_qos, properties=None):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+# Message to be published
+message = "Work harder, child!"
 
-# print message, useful for checking if it was successful
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
-# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
-# userdata is user defined data of any type, updated by user_data_set()
-# client_id is the given name of the client
-client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker with result code " + str(rc))
+    client.subscribe(topic2)
+
+
+def on_message(client, userdata, message):
+    print(f"Received message on topic {message.topic}")
+    if message.topic == topic2:
+        # Convert the received image data (payload) to a NumPy array
+        image_data = np.frombuffer(message.payload, dtype=np.uint8)
+        # Decode the image data to an OpenCV image
+        image = cv.imdecode(image_data, cv.IMREAD_COLOR)
+        # Display the image (you can modify this part based on your requirements)
+        cv.imshow("Received Image", image)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+
+# Create an MQTT client instance
+client = mqtt.Client()
+
+# Set the callback functions
 client.on_connect = on_connect
-
-# enable TLS for secure connection
-client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-# set username and password
-client.username_pw_set("Mattl", "Amdusias345")
-# connect to HiveMQ Cloud on port 8883 (default for MQTT)
-client.connect("50da3d644a6e4eb3ab1528547a0e5e02.s2.eu.hivemq.cloud", 8883)
-
-# setting callbacks, use separate functions like above for better visibility
-client.on_subscribe = on_subscribe
 client.on_message = on_message
-client.on_publish = on_publish
 
-# subscribe to all topics of encyclopedia by using the wildcard "#"
-client.subscribe("steuereinheit/#", qos=1)
+# Connect to the broker
+client.connect(broker_address, port, 60)
 
-# a single publish, this can also be done in loops, etc.
-client.publish("steuereinheit/bodenkamera", payload="hot", qos=1)
+# Loop to maintain the connection and handle messages
+client.loop_start()
 
-# loop_forever for simplicity, here you need to stop the loop manually
-# you can also use loop_start and loop_stop
-client.loop_forever()
+# Publish a message to topic1
+client.publish(topic1, message, qos=1)
+
+# Example: Wait for user input to exit the script
+input("Press Enter to exit...\n")
+
+# Stop the MQTT client loop and disconnect from the broker
+client.loop_stop()
+client.disconnect()
